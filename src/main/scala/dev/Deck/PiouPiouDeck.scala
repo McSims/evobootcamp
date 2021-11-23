c6f1f2fa537a9e42ab18560d1bf907639fff4fb4
Maksims Puskels <maksims.puskels@ui.com>
2021-11-23 20:18:11 +0200

    Complete implementation of deck.

---
+++ src/main/scala/dev/Deck/PiouPiouDeck.scala
@@ -0,0 +1,49 @@
+package Deck
+
+import scala.util.Random
+import java.{util => ju}
+import scala.annotation.tailrec
+
+abstract class Card
+
+// todo: started with trait for deck but couldnt finish implementing it. As need to return instance itself.
+case class Deck(cards: List[Card], trashCards: List[Card]) {
+
+  def dealCards(
+      numberOfPlayers: Int,
+      numberOfCards: Int
+  ): (Option[List[List[Card]]], Deck) = {
+    // Check if we have enough cards for players
+    val isEnoughCardsForDeal = numberOfCards * numberOfPlayers > cards.length
+    // todo: extract game rules from the deck... Deck should be reusable for any game.
+    val isNotWithinPlayersLimit = (2 until 6).contains(numberOfPlayers) == false
+    if (isEnoughCardsForDeal || isNotWithinPlayersLimit) {
+      (Option.empty, this)
+    } else {
+      val slices =
+        cards.sliding(numberOfCards, numberOfCards).toList.take(numberOfPlayers)
+      val newCards = cards.drop(numberOfPlayers * numberOfCards)
+      val deck = Deck(newCards, trashCards)
+      (Option(slices), deck)
+    }
+  }
+
+  def exchange(card: Card): (Card, Deck) = {
+    val newTrash = trashCards :+ card
+    if (cards.isEmpty) {
+      val shuffledTrash = shuffle(newTrash)
+      val newCard = shuffledTrash.head
+      val newCards = shuffledTrash.drop(1)
+      (newCard, Deck(newCards, List()))
+    } else {
+      val newCard = cards.head
+      val newCards = cards.drop(1)
+      (newCard, Deck(newCards, newTrash))
+    }
+  }
+
+  def shuffle(cards: List[Card]): List[Card] = {
+    Random.shuffle(cards)
+  }
+
+}
--- src/test/scala/DeckSpec.scala
+++ src/test/scala/DeckSpec.scala
@@ -1,17 +1,65 @@
 import org.scalatest.flatspec.AnyFlatSpec
-import Deck.DeckImpl
+import Deck.Deck
 import Card.PiouPiouCards
 
 class DeckSpec extends AnyFlatSpec {
-  "A deck" should "shuffle cards" in {
+
+  def fullDeck: Deck = {
     val cards = PiouPiouCards.allAvailableCards
-    val deck = new DeckImpl(cards)
-    deck.shuffle
-    assert(deck.numberOfCardsInDeck == 31)
-    for (i <- 0 to 30) {
-      val card = deck.dealNextCard
-      print(card)
-    }
-    // assert(deck.numberOfCardsInDeck == 1)
+    Deck(cards, List())
   }
+
+  def shortDeck = Deck(
+    List(PiouPiouCards.rooster, PiouPiouCards.fox, PiouPiouCards.chicken),
+    List()
+  )
+
+  "A deck" should "deal cards" in {
+    assert(fullDeck.cards.length == 31)
+    val dealtCards = fullDeck.dealCards(3, 5)
+    assert(dealtCards._1.get.length == 3)
+    assert(dealtCards._1.get(0).length == 5)
+    assert(dealtCards._1.get(1).length == 5)
+    assert(dealtCards._1.get(2).length == 5)
+  }
+
+  it must "deal only for 2 to 5 players" in {
+    assert(fullDeck.dealCards(1, 5)._1 == Option.empty)
+    assert(fullDeck.dealCards(2, 5)._1 != Option.empty)
+    assert(fullDeck.dealCards(5, 5)._1 != Option.empty)
+    assert(fullDeck.dealCards(6, 5)._1 == Option.empty)
+  }
+
+  it should "return itself if not enough cards" in {
+    // todo: maybe we should rewrite to validation return or throw?
+    val dealtCards = shortDeck.dealCards(2, 3)
+    assert(dealtCards._1 == Option.empty)
+    assert(dealtCards._2 == shortDeck)
+  }
+
+  it should "exchange card" in {
+    val result = shortDeck.exchange(PiouPiouCards.nest)
+    assert(result._1 == PiouPiouCards.rooster)
+    assert(result._2.trashCards.length == 1)
+    assert(result._2.cards.length == 2)
+  }
+
+  it should "shuffle trash cards" in {
+    assert(shortDeck.shuffle(shortDeck.cards) != shortDeck.cards)
+  }
+
+  it should "shuffle trash cards when no cards for exchange" in {
+    var result = shortDeck.exchange(PiouPiouCards.nest)
+    result = result._2.exchange(PiouPiouCards.nest)
+    result = result._2.exchange(PiouPiouCards.nest)
+    assert(result._1 == PiouPiouCards.chicken)
+    assert(result._2.trashCards.length == 3)
+    assert(result._2.cards.isEmpty)
+    result = result._2.exchange(PiouPiouCards.nest)
+    assert(result._1 == PiouPiouCards.nest)
+    assert(result._2.trashCards.isEmpty)
+    assert(result._2.cards.length == 3)
+    result._2.cards.foreach((card) => assert(card == PiouPiouCards.nest))
+  }
+
 }
